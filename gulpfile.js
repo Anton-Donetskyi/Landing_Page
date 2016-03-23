346844d55312582a0bea2571e906b5f009e48b1b
Anton Donetskyi <toxa.donec@yandex.ru>
2016-03-23 21:08:53 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	var gulp = require('gulp');	
+
+	function defaultTask() {
+		
+	}												// Initialization of the plugin
+
+	gulp.task('default', defaultTask) {
+					// Task body goes here
+	});
+}());	
